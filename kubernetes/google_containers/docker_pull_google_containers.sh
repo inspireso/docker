264361a93467758c795873e27f1a363ba07e6597
lanxenet <lanxenet@gmail.com>
2017-02-20 22:06:58 +0800

    Revert: feature: 更新版本

--- kubernetes/google_containers/docker_pull_google_containers.sh
+++ kubernetes/google_containers/docker_pull_google_containers.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-$ images=(kube-proxy-amd64:v1.5.3 kube-discovery-amd64:1.0 kubedns-amd64:1.9 kube-scheduler-amd64:v1.5.3 kube-controller-manager-amd64:v1.5.3 kube-apiserver-amd64:v1.5.3 etcd-amd64:3.0.14-kubeadm kube-dnsmasq-amd64:1.4 exechealthz-amd64:1.2 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.5.0 dnsmasq-metrics-amd64:1.0)
+images=(kube-discovery-amd64:1.0 kubedns-amd64:1.9 etcd-amd64:3.0.14-kubeadm kube-dnsmasq-amd64:1.4 exechealthz-amd64:1.2 pause-amd64:3.0 dnsmasq-metrics-amd64:1.0)
 for imageName in ${images[@]} ; do
-  docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/latest
+  docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
   docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/latest gcr.io/google_containers/$imageName
   docker rmi registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
+done
+
+images=(kube-proxy-amd64 kube-scheduler-amd64 kube-controller-manager-amd64 kube-apiserver-amd64)
+for imageName in ${images[@]} ; do
+  docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest
+  docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest gcr.io/google_containers/$imageName:v1.5.3
+  docker rmi registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest
 done

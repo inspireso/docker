f966bf71ddbe3a60a3c48c4c56e3d54a2e85b074
lanxenet <lanxenet@gmail.com>
2017-03-08 10:42:24 +0800

    update: v1.5.3

--- kubernetes/google_containers/docker_pull_google_containers.sh
+++ kubernetes/google_containers/docker_pull_google_containers.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-$ images=(kube-proxy-amd64:v1.5.1 kube-discovery-amd64:1.0 kubedns-amd64:1.9 kube-scheduler-amd64:v1.5.1 kube-controller-manager-amd64:v1.5.1 kube-apiserver-amd64:v1.5.1 etcd-amd64:3.0.14-kubeadm kube-dnsmasq-amd64:1.4 exechealthz-amd64:1.2 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.5.0 dnsmasq-metrics-amd64:1.0)
+$ images=(kube-proxy-amd64:v1.5.3 kube-controller-manager-amd64:v1.5.3  kube-scheduler-amd64:v1.5.3 kube-apiserver-amd64:v1.5.3 etcd-amd64:3.0.14-kubeadm  kube-discovery-amd64:1.0 pause-amd64:3.0)
 for imageName in ${images[@]} ; do
   docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
   docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName gcr.io/google_containers/$imageName

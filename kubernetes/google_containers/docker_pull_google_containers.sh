7a150afa7e923ca2f923fb09961dd2671a3608e4
lanxenet <lanxenet@gmail.com>
2017-03-13 23:54:57 +0800

    feature: 添加 readme

--- kubernetes/google_containers/docker_pull_google_containers.sh
+++ kubernetes/google_containers/docker_pull_google_containers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#master
 images=(kube-proxy-amd64 kube-controller-manager-amd64  kube-scheduler-amd64 kube-apiserver-amd64)
 for imageName in ${images[@]} ; do
   docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest
@@ -7,7 +8,16 @@ for imageName in ${images[@]} ; do
   docker rmi registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest
 done
 
-images=(etcd-amd64:3.0.14-kubeadm  kube-discovery-amd64:1.0 pause-amd64:3.0 etcd:2.2.1)
+#master
+images=(etcd-amd64:3.0.14-kubeadm  kube-discovery-amd64:1.0 pause-amd64:3.0 etcd:2.2.1 dnsmasq-metrics-amd64:1.0)
+for imageName in ${images[@]} ; do
+  docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
+  docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName gcr.io/google_containers/$imageName
+  docker rmi registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
+done
+
+#node
+images=(kube-proxy-amd64:v1.5.3 pause-amd64:3.0)
 for imageName in ${images[@]} ; do
   docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
   docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName gcr.io/google_containers/$imageName

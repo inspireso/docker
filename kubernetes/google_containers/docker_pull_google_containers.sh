61aa28a70a73d91ce937cf9fcd2616ba593c50ce
lanxenet <lanxenet@gmail.com>
2017-03-13 13:03:30 +0800

    feature: 添加 gcr.io/google_containers/etcd:2.2.1

--- kubernetes/google_containers/docker_pull_google_containers.sh
+++ kubernetes/google_containers/docker_pull_google_containers.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-$ images=(kube-proxy-amd64:v1.5.3 kube-controller-manager-amd64:v1.5.3  kube-scheduler-amd64:v1.5.3 kube-apiserver-amd64:v1.5.3 etcd-amd64:3.0.14-kubeadm  kube-discovery-amd64:1.0 pause-amd64:3.0)
+
+images=(kube-proxy-amd64 kube-controller-manager-amd64  kube-scheduler-amd64 kube-apiserver-amd64)
+for imageName in ${images[@]} ; do
+  docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest
+  docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest gcr.io/google_containers/$imageName:v1.5.3
+  docker rmi registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName:latest
+done
+
+images=(etcd-amd64:3.0.14-kubeadm  kube-discovery-amd64:1.0 pause-amd64:3.0 etcd:2.2.1)
 for imageName in ${images[@]} ; do
   docker pull registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName
   docker tag registry.cn-hangzhou.aliyuncs.com/kube_containers/$imageName gcr.io/google_containers/$imageName

2e8e094a58931876d5fe23145b5838cc534e54a0
enoch <lanxenet@gmail.com>
2023-06-15 14:42:35 +0800

    fix: 添加 ajdk

--- java/rootfs/run.sh
+++ java/rootfs/run.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-RESERVED_MEGABYTES=768
-limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
-heap_size=$(echo $JAVA_OPTS | grep Xmx)
-
-# If not default limit_in_bytes in cgroup
-if [ "$limit_in_bytes" -ne "9223372036854771712"  ] && [ -z $heap_size ]
-then
-    limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
-    heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
-    export JAVA_OPTS="-Xmx${heap_size}m -Xms${heap_size}m $JAVA_OPTS"
-    echo JAVA_OPTS=$JAVA_OPTS
-fi
-
 cd /app
 
 export JAVA_OPTS="$JAVA_OPTS \
